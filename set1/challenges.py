226d54b945477e3b9597772a4e89e087ec182715
tsondt <tsondt@gmail.com>
2016-09-26 18:48:04 -0400

    Challenge 1

---
+++ set1/challenges.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#Challenge 1
+import base64
+s = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+o = base64.b64encode(s.decode('hex'))
+assert o == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+print 's = ', s
+print 'o = ', o
+
+#Challenge 2
